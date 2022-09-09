50a3990927c0139a933828ccac17b7404ba12fcb
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-09-09 13:54:48 +0300

    Data Fill part is added

---
+++ DataFillingPoject/DataFillingPoject/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+
+namespace DataFillingPoject
+{
+    class Program
+    {
+        //user, operation, result(success, warning or fail), datetime(random), guid
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            string user, operation, result, guid;
+            DateTime dateTime;
+            List<string> users = new List<string>{ "Viktor", "Hristo", "Slavi", "Vilian", "Martin", "Daniel" };
+            List<string> operations = new List<string> { "Delete", "Add", "Update", "View" };
+            List<string> results = new List<string> { "Success", "Warning", "Fail"};
+            var ai = new TelemetryClient(new Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration() { InstrumentationKey = "e4259d2b-709c-4167-8900-71dd5c51a453" })
+            {
+                InstrumentationKey = "e4259d2b-709c-4167-8900-71dd5c51a453"
+            };
+            for(int i = 0; i < 200; i++)
+            {
+                dateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day - 1, random.Next(0, 23), random.Next(0, 59), random.Next(0, 59), random.Next(0, 999));
+                user = users[random.Next(users.Count)];
+                operation = operations[random.Next(operations.Count)];
+                result = results[random.Next(results.Count)];
+                guid = Guid.NewGuid().ToString();
+                ai.TrackEvent($"Index", new Dictionary<string, string>() { { "User", user }, { "Operation", operation }, { "Result", result }, { "Date", dateTime.ToString() }, { "Guid", guid } });
+            }
+            ai.Flush();
+        }
+    }
+}
