916065a24636619a31bdd8b7b294a73b0e33ea0f
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-09-12 13:12:42 +0300

    Search work but without time filter

--- DataManagementAndDisplay/DataManagementAndDisplay/Controllers/HomeController.cs
+++ DataManagementAndDisplay/DataManagementAndDisplay/Controllers/HomeController.cs
@@ -7,9 +7,9 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using DataManagementAndDisplay.Models;
 using Microsoft.Azure.ApplicationInsights.Query;
-using System.Threading;
+using Microsoft.AspNetCore.Http;
 
-namespace DataManagementAndDisplay.Controllers
+namespace Controllers
 {
     public class HomeController : Controller
     {
@@ -40,12 +40,78 @@ namespace DataManagementAndDisplay.Controllers
                 "Guid = customDimensions.Guid";
             var response =  await applicationInsightsClient.Query.ExecuteWithHttpMessagesAsync(applicationId, query);
             IEnumerable<IDictionary<string, object>> data = response.Body.Results;
-            return View(response.Body.Results);
+            return View(data);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Search()
         {
-            return View();
+            return View(new SearchModel { Time = "", User = "", Operation = "", Result = "" });
+        }
+        public async Task<IActionResult> SearchResult(IFormCollection collection)
+        {
+            var user = Convert.ToString(collection["User"]);
+            string operation = Convert.ToString(collection["Operation"]);
+            string result = Convert.ToString(collection["Result"]);
+            string time = Convert.ToString(collection["Time"]);
+            var ai = new TelemetryClient(new Microsoft.ApplicationInsights.Extensibility.TelemetryConfiguration() { InstrumentationKey = "e4259d2b-709c-4167-8900-71dd5c51a453" })
+            {
+                InstrumentationKey = "e4259d2b-709c-4167-8900-71dd5c51a453"
+            };
+
+            string applicationId = "a52e0aef-82df-4a0d-bdb2-3523c8c35aea";
+            string key = "aj5xbtiii1ejyszyftbie3xzss2ogto82bs8jj1d";
+            var credentials = new ApiKeyClientCredentials(key);
+            var applicationInsightsClient = new ApplicationInsightsDataClient(credentials);
+            var query = "customEvents " +
+               "| where timestamp > ago(8h) ";
+            if (user != "" && operation != "" && result != "")
+            {
+                query +=
+               $"| where customDimensions.User == '{user}' " +
+               $"| where customDimensions.Operation == '{operation}' " +
+               $"| where customDimensions.Result == '{result}' ";
+            }
+             else if (user == "" && operation != "" && result != "")
+            {
+                query +=
+               $"| where customDimensions.Operation == '{operation}' " +
+               $"| where customDimensions.Result == '{result}' ";
+            }
+            else if (user != "" && operation == "" && result != "")
+            {
+                query +=
+               $"| where customDimensions.User == '{user}' " +
+               $"| where customDimensions.Result == '{result}' ";
+            }
+            else if (user != "" && operation != "" && result == "")
+            {
+                query +=
+               $"| where customDimensions.User == '{user}' " +
+               $"| where customDimensions.Operation == '{operation}' ";
+            }
+            else if (user == "" && operation == "" && result != "")
+            {
+                query +=
+               $"| where customDimensions.Result == '{result}' ";
+            }
+            else if (user != "" && operation == "" && result == "")
+            {
+                query +=
+               $"| where customDimensions.User == '{user}' ";
+            }
+            else if (user == "" && operation != "" && result == "")
+            {
+                query +=
+               $"| where customDimensions.Operation == '{operation}' ";
+            }
+            query += "| project Date = customDimensions.Date, " +
+               "User = customDimensions.User, " +
+               "Result = customDimensions.Result, " +
+               "Operation = customDimensions.Operation, " +
+               "Guid = customDimensions.Guid";
+            var response = await applicationInsightsClient.Query.ExecuteWithHttpMessagesAsync(applicationId, query);
+            IEnumerable<IDictionary<string, object>> data = response.Body.Results;
+            return View("Index",data);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DataManagementAndDisplay/DataManagementAndDisplay/Controllers/SearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Controllers
+{
+    public class SearchModel
+    {
+        public string Time { get; set; }
+        public string User { get; set; }
+        public string Operation { get; set; }
+        public string Result { get; set; }
+    }
+}
