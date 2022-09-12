74e7008dfcabdd2a202db0eb3906dd70a47a77c0
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-09-12 16:35:33 +0300

    almost ready just need css styling

--- DataManagementAndDisplay/DataManagementAndDisplay/Controllers/HomeController.cs
+++ DataManagementAndDisplay/DataManagementAndDisplay/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace Controllers
             var credentials = new ApiKeyClientCredentials(key);
             var applicationInsightsClient = new ApplicationInsightsDataClient(credentials);
             var query = "customEvents " +
-                "| where timestamp > ago(2h) " +
+                "| where timestamp > ago(5h) " +
                 "| project Date = customDimensions.Date, " +
                 "User = customDimensions.User, " +
                 "Result = customDimensions.Result, " +
@@ -38,25 +38,25 @@ namespace Controllers
                 "Guid = customDimensions.Guid";
             var response =  await applicationInsightsClient.Query.ExecuteWithHttpMessagesAsync(applicationId, query);
             IEnumerable<IDictionary<string, object>> data = response.Body.Results;
-            return View(data);
+            return View(new MultipleModels(data, new SearchModel { Time = "", User = "", Operation = "", Result = "", Guid = "" } ));
         }
 
-        public IActionResult Search()
+       /* public IActionResult Search()
         {
-            return View(new SearchModel { Time = "", User = "", Operation = "", Result = "", Guid = "" });
-        }
+           // return View(new MultipleModels ( ));
+        }*/
         public async Task<IActionResult> SearchResult(IFormCollection collection)
         {
             string yesterday = $"{DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day - 1}";
             var credentials = new ApiKeyClientCredentials(key);
             var applicationInsightsClient = new ApplicationInsightsDataClient(credentials);
-            var user = Convert.ToString(collection["User"]);
-            string operation = Convert.ToString(collection["Operation"]);
-            string result = Convert.ToString(collection["Result"]);
-            string time = Convert.ToString(collection["Time"]);
-            string guid = Convert.ToString(collection["Guid"]);
+            var user = Convert.ToString(collection["searchModel.User"]);
+            string operation = Convert.ToString(collection["searchModel.Operation"]);
+            string result = Convert.ToString(collection["searchModel.Result"]);
+            string time = Convert.ToString(collection["searchModel.Time"]);
+            string guid = Convert.ToString(collection["searchModel.Guid"]);
             var query = "customEvents " +
-               "| where timestamp > ago(2h) ";
+               "| where timestamp > ago(5h) ";
             if (user != "")
             {
                 query +=
@@ -114,7 +114,7 @@ namespace Controllers
                "Guid = customDimensions.Guid";
             var response = await applicationInsightsClient.Query.ExecuteWithHttpMessagesAsync(applicationId, query);
             IEnumerable<IDictionary<string, object>> data = response.Body.Results;
-            return View("Index",data);
+            return View("Index",new MultipleModels(data));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DataManagementAndDisplay/DataManagementAndDisplay/Controllers/MultipleModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class MultipleModels
+    {
+        public IEnumerable<IDictionary<string, object>> Data;
+        public SearchModel searchModel;
+        public MultipleModels(IEnumerable<IDictionary<string, object>> data)
+        {
+            Data = data;
+        }
+        public MultipleModels(SearchModel data)
+        {
+            searchModel = data;
+        }
+        public MultipleModels(IEnumerable<IDictionary<string, object>> data, SearchModel data1)
+        {
+            Data = data;
+            searchModel = data1;
+        }
+    }
+}
