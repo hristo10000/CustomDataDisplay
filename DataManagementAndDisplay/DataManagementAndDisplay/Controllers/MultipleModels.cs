b60874ccde01be95e28ba87267930c3cc0bde973
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-09-20 13:40:25 +0300

    minor code style changes

--- DataManagementAndDisplay/DataManagementAndDisplay/Controllers/MultipleModels.cs
+++ DataManagementAndDisplay/DataManagementAndDisplay/Controllers/MultipleModels.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Controllers
 {
@@ -23,6 +20,5 @@ namespace Controllers
             Data = data;
             searchModel = data1;
         }
-
     }
 }
