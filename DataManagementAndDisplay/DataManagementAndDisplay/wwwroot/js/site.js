18dda2e97da24cea1ad09331ab861ea1f6a126f8
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-10-17 17:02:24 +0300

    ultra changes

--- DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/site.js
+++ DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/site.js
@@ -1,5 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+
+$("table").mousedown(function (ev) {
+    if (ev.which == 3) {
+        window.alert("opaaa");
+    }
+});
+
 $(document).ready(function () {
     $("#form").submit(function (event) {
         event.preventDefault();
