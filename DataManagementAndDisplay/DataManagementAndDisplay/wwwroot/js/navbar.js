3d5cbb2b1e8bb7ccf4dd20dec10aa0c4e3c5c91d
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-11-07 15:13:12 +0200

    codestyle

--- DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/navbar.js
+++ DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/navbar.js
@@ -1,21 +1,21 @@
 ﻿var isNavbarOpen = false;
 var eventIndex = 0;
 
-$("html").mousedown(function (ev) {
+$('html').mousedown(function (ev) {
     if (eventIndex == 1) {
         eventIndex = 0;
         return;
     }
     if (isNavbarOpen) {
         switch (ev.target.classList[1]) {
-            case "navbar-option-view-model":
+            case 'navbar-option-view-model':
                 if (ev.which == 1)
                 {
                     showView('page1');
-                    $(".page1").addClass("currently-shown-page");
-                    $(".navbar-option-view-model").text("➧View Model");
-                    $(".page2").removeClass("currently-shown-page");
-                    $(".navbar-option-create-model").text("Create Model");
+                    $('.page1').addClass('currently-shown-page');
+                    $('.navbar-option-view-model').text('➧View Model');
+                    $('.page2').removeClass('currently-shown-page');
+                    $('.navbar-option-create-model').text('Create Model');
                     const elements = document.getElementsByClassName('model');
                     for (var i = 0; i < elements.length; i++) {
                         elements[i].parentNode.removeChild(elements[i]);
@@ -23,13 +23,13 @@ $("html").mousedown(function (ev) {
                 }
                 
                 break;
-            case "navbar-option-create-model":
+            case 'navbar-option-create-model':
                 if (ev.which == 1) {
                     showView('page2');
-                    $(".page2").addClass("currently-shown-page");
-                    $(".navbar-option-view-model").text("View Model");
-                    $(".page1").removeClass("currently-shown-page");
-                    $(".navbar-option-create-model").text("➧Create Model");
+                    $('.page2').addClass('currently-shown-page');
+                    $('.navbar-option-view-model').text('View Model');
+                    $('.page1').removeClass('currently-shown-page');
+                    $('.navbar-option-create-model').text('➧Create Model');
 
                     var allModels = GetAllModels();
                 }
@@ -37,32 +37,31 @@ $("html").mousedown(function (ev) {
             default:
                 break;
         }
-        $(".navbar").css("display", "none");
-        $(".navbar-button").css("display", "flex");
+        $('.navbar').css('display', 'none');
+        $('.navbar-button').css('display', 'flex');
         isNavbarOpen = false;
     } else if (isCustomContextMenuOpened && ev.which == 1) {
         switch (ev.target.classList[1]) {
-            case "context-menu-option-fill-in-table":
+            case 'context-menu-option-fill-in-table':
                     isCustomContextMenuFillInTableOptionSelected = true;
                     $(targetTableCell).click();
                 break;
-            case "context-menu-option-go-to-the-top":
+            case 'context-menu-option-go-to-the-top':
                 window.scrollTo(0, 0);
                 break;
             default:
                 break;
         }
-        $(".custom-context-menu-wrapper").css("display", "none");
-        $(targetTableCell).removeClass("current-target-for-context-menu");
+        $('.custom-context-menu-wrapper').css('display', 'none');
+        $(targetTableCell).removeClass('current-target-for-context-menu');
         isCustomContextMenuOpened = false;
     }
 });
 
-$(".navbar-button").mousedown(function (ev) {
-    
+$('.navbar-button').mousedown(function (ev) {
     if (ev.which != 1) return;
-    $(".navbar-button").css("display", "none");
-    $(".navbar").css("display", "flex");
+    $('.navbar-button').css('display', 'none');
+    $('.navbar').css('display', 'flex');
     isNavbarOpen = true;
     eventIndex = 1;
 });
