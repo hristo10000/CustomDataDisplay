45cf0fff2c4e8d05b59ba6a5812138108e6833e0
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-11-07 14:29:03 +0200

    add text column to create model form is working

---
+++ DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/create-model.js
@@ -0,0 +1,7 @@
+﻿function AddTextColumn() {
+    var textColumn = '<div class="field-column-input">' +
+        '<input type="text" class="new-model-dispay-name" for="new-model-dispay-name" placeholder="Display Name"/>' +
+        '<input type="text" class="new-model-internal-name" for="new-model-internal-name" placeholder="Internal Name"/>' +
+        '</div>';
+    $('.added-columns-list').append(textColumn);
+}
