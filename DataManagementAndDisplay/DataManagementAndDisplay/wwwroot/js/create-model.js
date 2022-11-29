59d9d24abc3183d0b57a8bf2823195d7951d2a8f
ViktorMilanov <101833400+ViktorMilanov@users.noreply.github.com>
2022-11-29 15:40:42 +0200

    nai-nakraq

--- DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/create-model.js
+++ DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/create-model.js
@@ -15,16 +15,20 @@ function CreateModel() {
     for (var i = 0; i < inputs.length; i++) {
         var field = inputs[i];  
         Model.Fields[i] = {};
-        for (var j = 0; j < field.children.length - 1; j++) {     
+        for (var j = 0; j < field.children.length; j++) {     
             var input = field.children[j].value;
             if (j == 0) {
                 Model.Fields[i].DisplayName = input;
             } else if (j == 1) {
                 Model.Fields[i].InternalName = input;
-            } else if (j == 2) {
+            } else if (j == 2) { 
+                input = field.children[j];
                 Model.Fields[i].PossibleValues = [];
-                Model.Fields[i].PossibleValues[0] = {};
-                Model.Fields[i].PossibleValues[0].PossibleOptionValue = input;
+                for (var k = 1; k < input.children.length; k++) {
+                    value = input.children[k].value;
+                    Model.Fields[i].PossibleValues[Model.Fields[i].PossibleValues.length] = {};
+                    Model.Fields[i].PossibleValues[Model.Fields[i].PossibleValues.length - 1].PossibleOptionValue = value;
+                }
             }
             else {
                 Model.Fields[i].PossibleValues[Model.Fields[i].PossibleValues.length] = {};
@@ -48,7 +52,7 @@ function CreateModel() {
 function AddTextColumn() {
     numberOfFields = $('.field-column-input').length;
     var divForTextColumn = $('<div></div>').addClass("field-column-input").attr("id", `field-column-input${numberOfFields}`);
-    var inputForDisplayName = $('<input></input>').addClass("new-model-dispay-name").attr("type", "text").attr("placeholder", "Display Name");
+    var inputForDisplayName = $('<input></input>').addClass("new-model-dispay-name").attr("type", "text").attr("placeholder", "Display Name").attr('required', '');
     var inputForInternalName = $('<input></input>').addClass("new-model-internal-name").attr("type", "text").attr("placeholder", "Internal Name");
     var divForNewColumn = $('<div></div>').addClass("add-new-enum-column").attr("id", `add-new-enum-column${numberOfFields}`).attr("onclick", "AddOptionValues(this.id)").text("+ Add Possible Values");
     divForTextColumn.append(inputForDisplayName);
