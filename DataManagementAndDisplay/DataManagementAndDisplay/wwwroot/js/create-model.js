53f09287729366c8872e602f01a03d89b079bf4a
Hristo Ivanov <103217093+hristo10000@users.noreply.github.com>
2022-11-30 14:18:20 +0200

    codestyle

--- DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/create-model.js
+++ DataManagementAndDisplay/DataManagementAndDisplay/wwwroot/js/create-model.js
@@ -52,10 +52,10 @@ function CreateModel() {
 
 function AddTextColumn() {
     numberOfFields = $('.field-column-input').length;
-    var divForTextColumn = $('<div></div>').addClass("field-column-input").attr("id", `field-column-input${numberOfFields}`);
-    var inputForDisplayName = $('<input></input>').addClass("new-model-dispay-name").attr("type", "text").attr("placeholder", "Display Name").attr('required', '');
-    var inputForInternalName = $('<input></input>').addClass("new-model-internal-name").attr("type", "text").attr("placeholder", "Internal Name");
-    var divForNewColumn = $('<div></div>').addClass("add-new-enum-column").attr("id", `add-new-enum-column${numberOfFields}`).attr("onclick", "AddOptionValues(this.id)").text("+ Add possible values");
+    var divForTextColumn = $('<div></div>').addClass('field-column-input').attr('id', `field-column-input${numberOfFields}`);
+    var inputForDisplayName = $('<input></input>').addClass('new-model-dispay-name').attr('type', 'text').attr('placeholder', 'Display Name').attr('required', '');
+    var inputForInternalName = $('<input></input>').addClass('new-model-internal-name').attr('type', 'text').attr('placeholder', 'Internal Name');
+    var divForNewColumn = $('<div></div>').addClass('add-new-enum-column').attr('id', `add-new-enum-column${numberOfFields}`).attr('onclick', 'AddOptionValues(this.id)').text('+ Add possible values');
     divForTextColumn.append(inputForDisplayName);
     divForTextColumn.append(inputForInternalName);
     divForTextColumn.append(divForNewColumn);
@@ -67,9 +67,9 @@ function AddOptionValues(id) {
     var div = button.parent();
     var idOfDiv = div.attr('id')
     button.remove();
-    var divForValues = $('<div></div>').addClass("values-list").attr("id", `values-list-${id}`);
-    var divForNewValue = $('<div></div>').addClass("add-new-enum-column").attr("id", `add-new-option-${idOfDiv}`).attr("onclick", "AddNewOption(this.id)").text("+ Add a possible value");
-    var inputForValue = $('<input></input>').addClass("new-model-value").attr("type", "text").attr("placeholder", "Value").attr('required', '');
+    var divForValues = $('<div></div>').addClass('values-list').attr('id', `values-list-${id}`);
+    var divForNewValue = $('<div></div>').addClass('add-new-enum-column').attr('id', `add-new-option-${idOfDiv}`).attr('onclick', 'AddNewOption(this.id)').text('+ Add a possible value');
+    var inputForValue = $('<input></input>').addClass('new-model-value').attr('type', 'text').attr('placeholder', 'Value').attr('required', '');
     divForValues.append(divForNewValue);
     divForValues.append(inputForValue);
     div.append(divForValues);
@@ -78,25 +78,25 @@ function AddOptionValues(id) {
 function AddNewOption(id) {
     var button = $(`#${id}`);
     var div = button.parent();
-    var inputForValue = $('<input></input>').addClass("new-model-value").attr("type", "text").attr("placeholder", "Value");
+    var inputForValue = $('<input></input>').addClass('new-model-value').attr('type', 'text').attr('placeholder', 'Value');
     div.append(inputForValue);
 }
 
 function ResetForm() {
     var form = $('#create-model-form').empty();
-    var divForNameAndDescription = $('<div></div>').addClass("create-model-form-element-for-name-and-password");
-    var inputForName = $('<input></input>').addClass("new-model-name").attr("type", "text").attr("placeholder", "Name").attr('required', '');
-    var inputForDescription = $('<input></input>').addClass("new-model-description").attr("type", "text").attr("placeholder", "Description").attr('required', '');
-    var resetButton = $('<button></button>').attr('type', 'button').addClass("clear-create-model-form-button").attr("onclick", "ConfirmResetModelForm('Reset Form', 'Are you sure you want to COMPLETELY RESET this form? This action CANNOT be undone!', 'Yes', 'No')").text("Reset");
+    var divForNameAndDescription = $('<div></div>').addClass('create-model-form-element-for-name-and-password');
+    var inputForName = $('<input></input>').addClass('new-model-name').attr('type', 'text').attr('placeholder', 'Name').attr('required', '');
+    var inputForDescription = $('<input></input>').addClass('new-model-description').attr('type', 'text').attr('placeholder', 'Description').attr('required', '');
+    var resetButton = $('<button></button>').attr('type', 'button').addClass('clear-create-model-form-button').attr('onclick', 'ConfirmResetModelForm("Reset Form", "Are you sure you want to COMPLETELY RESET this form? This action CANNOT be undone!", "Yes", "No")').text('Reset');
     var resetButtonDiv = $('<div></div>');
     resetButtonDiv.append(resetButton);
     divForNameAndDescription.append(inputForName);
     divForNameAndDescription.append(inputForDescription);
     form.append(divForNameAndDescription);
-    var divForCreateFields = $('<div></div>').addClass("create-model-form-element-for-custom-fields");
-    var divForAddedColumns = $('<div></div>').addClass("added-columns-list");
-    var divForNewTextColumn = $('<div></div>').addClass("add-new-text-column").attr("onclick", "AddTextColumn()").text("+ New Column");
-    var inputForSubmit = $('<input></input>').attr("type", "submit").attr("value", "Create Model").addClass('submit-button-for-create-model');
+    var divForCreateFields = $('<div></div>').addClass('create-model-form-element-for-custom-fields');
+    var divForAddedColumns = $('<div></div>').addClass('added-columns-list');
+    var divForNewTextColumn = $('<div></div>').addClass('add-new-text-column').attr('onclick', 'AddTextColumn()').text('+ New Column');
+    var inputForSubmit = $('<input></input>').attr('type', 'submit').attr('value', 'Create Model').addClass('submit-button-for-create-model');
     divForCreateFields.append(divForAddedColumns);
     divForCreateFields.append(divForNewTextColumn);
     form.append(divForCreateFields);
@@ -105,22 +105,22 @@ function ResetForm() {
 }
 
 function ConfirmResetModelForm(title, msg, $true, $false) {
-    var mainDiv = $("<div></div>").addClass("dialog-ovelay");
-    var dialogDiv = $("<div></div>").addClass("dialog");
-    var header = $("<header></header>");
-    var h3 = $("<h3></h3>").text(title);
-    var i = $("<i></i>").addClass("fa fa-close");
+    var mainDiv = $('<div></div>').addClass('dialog-ovelay');
+    var dialogDiv = $('<div></div>').addClass('dialog');
+    var header = $('<header></header>');
+    var h3 = $('<h3></h3>').text(title);
+    var i = $('<i></i>').addClass('fa fa-close');
     header.append(i);
     header.append(h3);
     dialogDiv.append(header);
-    var div = $("<div></div>").addClass("dialog-msg");
-    var p = $("<p></p>").text(msg);
+    var div = $('<div></div>').addClass('dialog-msg');
+    var p = $('<p></p>').text(msg);
     div.append(p);
     dialogDiv.append(div);
-    var footer = $("<footer></footer>");
-    var divInFooter = $("<div></div>").addClass("controls");
-    var firstButton = $("<button></button>").addClass("button button-danger doAction btnEnable").attr("type", "button").text($true);
-    var secondButton = $("<button></button>").addClass("button button-default cancelAction").attr("type", "button").text($false);
+    var footer = $('<footer></footer>');
+    var divInFooter = $('<div></div>').addClass('controls');
+    var firstButton = $('<button></button>').addClass('button button-danger doAction btnEnable').attr('type', 'button').text($true);
+    var secondButton = $('<button></button>').addClass('button button-default cancelAction').attr('type', 'button').text($false);
     divInFooter.append(firstButton);
     divInFooter.append(secondButton);
     footer.append(divInFooter);
